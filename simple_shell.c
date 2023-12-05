6e5e88da32f1a02302502b9f2fefd131d6fe784d
isaac-ik <isaac.ikpemi@gmail.com>
2023-12-05 08:22:32 -0800

    simple shell 0.1

--- simple_shell.c
+++ simple_shell.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
 
 /**
  * main - entry point
@@ -12,6 +14,57 @@
  */
 int main(int argc, char **argv)
 {
+	char *cmdline;
+	size_t n = 10;
+	ssize_t st;
+	char **argV, **envp = {NULL};
+	int ch2, i = 1;
+	pid_t pid;
+	char *delim = " ";
 
+	cmdline = (char *)malloc(sizeof(char) * n);
+
+	/* Prompt */
+	printf("$ ");
+
+	st = getline(&cmdline, &n, stdin);
+	if (st == -1)
+	{
+		printf("Error 1\n");
+		return (-1);
+	}
+	/* Tokenizing the cmd */
+	argV = (char **)malloc(sizeof(char *) * 5);
+	argV[0] = strtok(cmdline, delim);
+	while (cmdline != NULL)
+	{
+		argV[i] = strtok(NULL, delim);
+		printf("the argument is %i is %s\n", i, argV[i]);
+		i++;
+	}
+	argV[i] = '\0';
+
+	/* create a child process */
+	pid = fork();
+	if (pid == -1)
+	{
+		printf("Error 3\n");
+		return (-1);
+	}
+	if (pid == 0)
+	{
+		/* exceute the prompt */
+		ch2 = execve(argV[0], argV, envp);
+		if (ch2 == -1)
+		{
+			printf("Error 2\n");
+			return (-1);
+		}
+	}
+	if (pid > 0)
+		wait(NULL);
+
+
+	free(cmdline);
 	return (0);
 }
