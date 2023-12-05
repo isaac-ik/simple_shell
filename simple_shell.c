c29264fa8cbab67bfb54ba2b7f486c451456c1f1
isaac-ik <isaac.ikpemi@gmail.com>
2023-12-05 07:07:50 -0800

    A simple code passing Betty requirment

---
+++ simple_shell.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Programmer: Isaac-ik
+ * Description: a UNIX command program/interpreter
+ * @argc: argumnet count
+ * @argv: argument vector
+ * Return: On sucess, 0. else -1
+ */
+int main(int argc, char **argv)
+{
+
+	return (0);
+}
